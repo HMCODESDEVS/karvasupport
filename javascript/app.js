90131adda866c8f2ee245e3845fde51953ed2574
RPEOP2026 <dev.gonzaloortizrobles@outlook.es>
2026-03-08 15:20:17 +0100

    Add files via upload

---
+++ javascript/app.js
@@ -0,0 +1,12 @@
+function webextraña(){
+    window.location.href = "https://karvasupport.netlify.app/supportservice/webextraña"
+}
+function nosaleurl(){
+    window.location.href = "https://karvasupport.netlify.app/supportservice/nosaleurl"
+}
+function descarga(){
+    window.location.href = "https://karvasupport.netlify.app/supportservice/descarga"
+}
+function nomeseguiar(){
+    window.location.href = "https://karvasupport.netlify.app/supportservice/nomeseguiar"
+}
